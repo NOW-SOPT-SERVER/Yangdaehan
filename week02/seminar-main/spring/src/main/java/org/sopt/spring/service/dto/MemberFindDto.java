16f3fd2fce35c0082dd5ecb668e776e027661aa7
Yangdaehan <yangdh08@naver.com>
2024-04-17 16:15:07 +0900

    feat : id 값 반환

--- week02/seminar-main/spring/src/main/java/org/sopt/spring/service/dto/MemberFindDto.java
+++ week02/seminar-main/spring/src/main/java/org/sopt/spring/service/dto/MemberFindDto.java
@@ -4,11 +4,12 @@ import org.sopt.spring.domain.Member;
 import org.sopt.spring.domain.Part;
 
 public record MemberFindDto(
+        Long id,
         String name,
         Part part,
         int age
 ) {
     public static MemberFindDto of(Member member) {
-        return new MemberFindDto(member.getName(), member.getPart(), member.getAge());
+        return new MemberFindDto(member.getId(), member.getName(), member.getPart(), member.getAge());
     }
 }
